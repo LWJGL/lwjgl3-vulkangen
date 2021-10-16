650cbaf8d605725fc43a91ce3ef2b081329678cf
Ioannis Tsakpinis <iotsakp@gmail.com>
2021-10-16 20:46:17 +0300

    fix: do not force pNext to be nullable
    The registry now contains correct optional attributes for pNext members.

--- src/main/kotlin/org/lwjgl/vulkangen/Registry.kt
+++ src/main/kotlin/org/lwjgl/vulkangen/Registry.kt
@@ -540,7 +540,7 @@ ${templateTypes.asSequence()
                             }
 
                         val nullable = if (
-                            (member.name == "pNext" || member.optional != null || (member.noautovalidity != null && member.len.any())) && 
+                            (member.optional != null || (member.noautovalidity != null && member.len.any())) && 
                             (member.indirection.isNotEmpty() || types.getValue(member.type).let { it is TypeFuncpointer || (it is TypeHandle && it.type == "VK_DEFINE_HANDLE") })
                         ) "nullable.." else ""
 
